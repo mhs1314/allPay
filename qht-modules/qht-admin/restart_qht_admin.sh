9797583559f262ff28e568e78b3639bc834d97f7
root <2466607841@qq.com>
2018-11-10 14:11:26 +0800

    启动脚本

---
+++ qht-modules/qht-admin/restart_qht_admin.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+JAVA_OPTS='-Xms128m -Xmx512m -XX:NewSize=128m -XX:MaxNewSize=512m -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=128m -XX:NewRatio=2 -XX:MaxTenuringThreshold=8 -XX:+DisableExplicitGC'
+dir=`cd "$( dirname "$0" )" && pwd`
+qht_jar=qht-admin.jar
+RESOURCE_NAME=${dir}/target/${qht_jar}
+LOG_NAME=/home/qht_adm.log
+NG="zh_CN.UTF-8"
+pid=`ps ax | grep qht-admin.jar | grep java | head -1 | awk '{print $1}'`
+echo $pid
+kill -9 $pid
+
+sleep 3
+echo '服务停止成功,开始重启服务...'
+
+nohup java -jar ${RESOURCE_NAME} >> ${LOG_NAME} 2>&1 &
+rm -rf tpid
+echo Start Success!
