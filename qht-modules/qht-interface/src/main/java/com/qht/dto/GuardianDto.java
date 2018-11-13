3b0b53417cc4195375a226fd8d3733f7741db50e
zjh <1805099660@qq.com>
2018-11-13 14:54:33 +0800

    1

--- qht-modules/qht-interface/src/main/java/com/qht/dto/GuardianDto.java
+++ qht-modules/qht-interface/src/main/java/com/qht/dto/GuardianDto.java
@@ -3,6 +3,8 @@ package com.qht.dto;
 import java.io.Serializable;
 
 public class GuardianDto  implements Serializable {
+    //监护人id
+    private String gid;
     //监护人关系
     private String call;
     //监护人电话
@@ -22,5 +24,13 @@ public class GuardianDto  implements Serializable {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public String getGid() {
+        return gid;
+    }
+
+    public void setGid(String gid) {
+        this.gid = gid;
     }
 }
