1f018fe63c12db7d9fa3c283a0ced6428cada0df
不忘 <zuoxh0703@163.com>
2018-11-16 13:53:01 +0800

    2018-11-16 1.52

--- qht-modules/qht-api/src/main/java/com/qht/rest/TeacherController.java
+++ qht-modules/qht-api/src/main/java/com/qht/rest/TeacherController.java
@@ -1,13 +1,35 @@
 package com.qht.rest;
 
+import com.qht.RequestObject;
+import com.qht.ResultObject;
+import com.qht.dto.IndexMyCourseDto;
+import com.qht.dto.IndexMyCourseParameter;
+import com.qht.dto.LoginInfoDto;
+import com.qht.services.TeacherService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.qht.biz.TeacherBiz;
 import com.qht.entity.Teacher;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("teacher")
-public class TeacherController extends APIBaseController<TeacherBiz,Teacher> {
+public class TeacherController extends APIBaseController<TeacherBiz,Teacher> implements TeacherService {
 
+    @Override
+    public ResultObject<String> login(RequestObject<LoginInfoDto> rquest) {
+        return null;
+    }
+
+    @Override
+    @PostMapping("indexMyCourse")
+    @ResponseBody
+    public ResultObject<List<IndexMyCourseDto>> indexMyCourse(RequestObject<IndexMyCourseParameter> requestObject) {
+
+        return null;
+    }
 }
