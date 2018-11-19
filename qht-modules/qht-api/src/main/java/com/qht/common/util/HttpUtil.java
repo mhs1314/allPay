d6322660fd8f44679808400d73f354f7d7876e73
root <2466607841@qq.com>
2018-11-20 00:22:28 +0800

    uid

---
+++ qht-modules/qht-api/src/main/java/com/qht/common/util/HttpUtil.java
@@ -0,0 +1,70 @@
+package com.qht.common.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpUtil {
+	public static String post(String url ,String param) {
+		PrintWriter out = null;
+        BufferedReader in = null;
+        String result = "";
+        try {
+            URL httpUrl = new URL(url);           
+            HttpURLConnection conn = (HttpURLConnection) httpUrl.openConnection();
+        	conn.setRequestMethod("POST");
+        	conn.setRequestProperty("Content-Type", "application/json");
+        	conn.setRequestProperty("connection", "keep-alive");
+        	conn.setUseCaches(false);//设置不要缓存
+        	conn.setInstanceFollowRedirects(true);
+        	conn.setDoOutput(true);
+            conn.setDoInput(true);
+        	conn.connect();            
+            out = new PrintWriter(conn.getOutputStream());           
+            out.print(param);            
+            out.flush();           
+            in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送 POST 请求出现异常！"+e);
+            e.printStackTrace();
+        }
+        //使用finally块来关闭输出流、输入流
+        finally{
+        	close(out,in);
+        }
+        return result;
+    } 
+	
+	
+    
+    private static void close(PrintWriter out,BufferedReader in) {
+    	try{
+            if(out!=null){
+                out.close();
+            }
+            if(in!=null){
+                in.close();
+            }
+        }
+        catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+		String url = "http://console.tim.qq.com/v4/group_open_http_svc/get_appid_group_list";
+		String param = "accesstoken=xxx&identifier=group_root&sdkappid=88888888&random=99999999&contenttype=json";
+		String result= post(url,param);
+		System.out.println(result);
+	
+	}
+}
